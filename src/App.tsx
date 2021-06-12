0fdbc34c285c60e81fa9660f728e8c53fbf9fdb5
meixiang474 <1436098850@qq.com>
2021-06-13 01:58:42 +0800

    chore: 引入字体图标

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { IconStyle } from "./assets/iconfont/iconfont";
+import { GlobalStyle } from "./style";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyle />
+      <IconStyle />
+      <i className="iconfont icon-jiantou1"></i>
     </div>
   );
 }
